79321773a9b8bbc9b9a0e2e83a12b4069bdf3899
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2022-11-27 16:33:29 +0000

    Converted term spawner to a posix shell compliant form

--- lib/Bindings/Core.hs
+++ lib/Bindings/Core.hs
@@ -9,23 +9,23 @@ import XMonad.Actions.WithAll (killOthers)
 import qualified Data.Map as M -- see XMonad.Doc.Extending
 import qualified XMonad.Actions.FlexibleManipulate as Flex
 
-
-
 core :: [(String, X())]
 core =
     [ ("M-e", spawn "nemo")
     , ("M-S-e", namedScratchpadAction scratchpads "nemo")
 
     -- , ("M-S-<Return>", spawn "kitty")
-    , ("M-S-<Return>", spawn "[[ $(($RANDOM % 2)) = 1 ]] && alacritty || kitty") -- spawn random terminal
+    -- , ("M-S-<Return>", spawn "[[ $(($RANDOM % 2)) = 1 ]] && alacritty || kitty") -- spawn random terminal
+
+    , ("M-S-<Return>", spawn "if [ $(shuf -i 0-1 -n 1) = 1 ]; then alacritty; else kitty; fi")
+      
     , ("M-x", bindFirst [(className =? "Emacs", pure ()), (pure True, kill)]) -- do not kill emacs by mistake with M-x
     , ("M-p", spawn "rofi -show drun")
 
     , ("M-S-k", killOthers)
     , ("M-C-p", spawn "polybar-msg cmd toggle")
     , ("M-S-l", spawn "brightnessctl -s set 5 && i3lock -ueni ~/Pictures/gem_full.png; brightnessctl -r")
-    , ("M-$", spawn "~/.config/rofi/powermenu.sh")
-      
+    , ("M-$", spawn "~/.config/rofi/powermenu.sh")      
 
     -- function keys
     , ("<XF86AudioMute>", spawn "pamixer -t")
