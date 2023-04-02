aa778326b1e9dd7ef2494fccdd912491928d5321
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-04-02 16:58:08 +0100

    Fix window hook to display Colemak keyboard correctly

--- lib/Hooks.hs
+++ lib/Hooks.hs
@@ -1,7 +1,7 @@
 module Hooks where
 
 import XMonad    
-import qualified XMonad.StackSet as W (swapUp)
+import qualified XMonad.StackSet as W (swapDown)
 import XMonad.Hooks.ManageHelpers (isDialog, isFullscreen, doCenterFloat, doFullFloat)
 import XMonad.Hooks.InsertPosition (insertPosition, Focus(Newer), Position(End))
 
@@ -14,14 +14,18 @@ import XMonad.Hooks.FadeWindows
 -- Make some apps (including gimp) float by default
 myManageHook :: ManageHook
 myManageHook = composeAll
-    [ className =? "Gimp-2.10" --> doCenterFloat -- find name with xprop
-    , isDialog --> doF W.swapUp
+    [ className =? "Gimp-2.10" --> doCenterFloat -- find name with xprop | grep 'CLASS'
+    , className =? "Gkbd-keyboard-display" --> doF W.swapDown
+                                
+    , isDialog --> doF W.swapDown -- prevent floating dialog from appearing below window
     , className =? "confirm" --> doFloat
     , className =? "file_progress" --> doFloat
     , className =? "download" --> doFloat
     , className =? "error" --> doFloat
     -- , isFullscreen -->  doFullFloat
+
     , insertPosition End Newer -- open new windows at the end
+                   
     -- Options: doFloat vs doCenterFloat
     -- Options for insertPosition: Focus (Newer/Older) and Position (Master, End, Above, Below)
     ]
@@ -31,10 +35,13 @@ myLogHook = refocusLastLogHook <> updatePointer (0.5, 0.5) (0.5, 0.5) <> fadeWin
 myHandleEventHook = refocusLastWhen refocusingIsActive <> fadeWindowsEventHook -- handleEventHook def
 
 -- this FadeHook requires Picom and serves to change its settings
+-- find a window className with xprop | grep 'CLASS'
 myFadeHook = composeAll
     [ transparency 0.1 -- default transparency
     , isUnfocused --> transparency 0.3
+    -- , (className =? "i3lock") --> opaque -- does not work
     -- , (title =? "Alacrity's") --> transparency 0.8
     -- , isFullscreen --> opaque
     , (className =? "Evince") --> opaque
+    , (className =? "Pix") --> opaque
     ]
