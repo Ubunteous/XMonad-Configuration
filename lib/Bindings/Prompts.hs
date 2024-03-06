012db21b8bbd7e756caf617f624d351d083de220
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-03-06 19:37:46 +0100

    Adding more prompts (man, shell, nix, zsh, duck)

--- lib/Bindings/Prompts.hs
+++ lib/Bindings/Prompts.hs
@@ -15,21 +15,22 @@ import qualified XMonad.Prompt as P
 import qualified XMonad.StackSet as W
 import qualified XMonad.Actions.Submap as SM
     
-prompt = [ ("M-r", promptSearch myPromptConfig google)
-         , ("M-g", promptSearch myPromptConfig google)
-         , ("M-;", promptSearch myPromptConfig google)
+prompt = [ ("M-h", promptSearch myPromptConfig duckEngine)
+         , ("M-f", shellPrompt myPromptConfig)
 
          , ("M-d", SM.visualSubmap def $ M.fromList $ map (\(k, s, a) -> ((0, k), (s, a)))
-                     [ (xK_q, "man", manPrompt myPromptConfig)
-                     , (xK_s, "shell", shellPrompt myPromptConfig)
-                     , (xK_d, "window", windowMultiPrompt myPromptConfig [(Goto, allWindows), (Bring, wsWindows), (Goto, wsWindows)])
-                     , (xK_f, "window -> ws", workspacePrompt myPromptConfig (windows . W.shift))
-                     , (xK_g, "xmonad", xmonadPrompt myPromptConfig)
-                     , (xK_h, "zsh", zshPrompt myPromptConfig "/etc/profiles/per-user/ubunteous/bin/zsh")
-                     , (xK_j, "internet", promptSearch myPromptConfig google)
+                     [ (xK_d, "drop window to ws #", workspacePrompt myPromptConfig (windows . W.shift))
+                     , (xK_g, "go to window", windowMultiPrompt myPromptConfig [(Goto, allWindows), (Bring, wsWindows), (Goto, wsWindows)])
+                     , (xK_i, "internet", promptSearch myPromptConfig duckEngine) -- google
 
                      -- Goto, Bring, BringCopy, BringToMaster, WithWindow String (Window -> X ())
                      , (xK_k, "bring window", windowPrompt myPromptConfig Bring allWindows) -- { P.autoComplete = Just 500000 }
+
+                     , (xK_m, "man", manPrompt myPromptConfig)
+                     , (xK_n, "nixpkgs search", promptSearch myPromptConfig nixEngine)
+                     , (xK_s, "shell", shellPrompt myPromptConfig)
+                     , (xK_x, "xmonad", xmonadPrompt myPromptConfig)
+                     , (xK_z, "zsh", zshPrompt myPromptConfig "/etc/profiles/per-user/ubunteous/bin/zsh")
                      ])]
          
 
@@ -54,3 +55,8 @@ myPromptConfig = def
                  , P.sorter = fuzzySort
                  , P.changeModeKey = xK_twosuperior -- xK_Super_L
                  }
+
+duckEngine = searchEngine "DuckDuckGo" "https://duckduckgo.com/?ks=s&k7=24273a&kae=d&kj=24273a&k9=8bd5ca&kaa=f5a97f&k21=363a4f&k1=-1&kak=-1&kax=-1&kaq=-1&kap=-1&kao=-1&kau=-1&q="
+
+nixEngine = searchEngine "Nix Package"
+            "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query="
